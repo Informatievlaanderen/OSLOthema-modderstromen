eac0a79927f5abf4ff46a9f1005cac3ffdb718ef
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-06-12 09:15:48 +0200

    Initialize config

---
+++ types/configuration.ts
@@ -0,0 +1,28 @@
+export interface Configuration {
+  title: string;
+  category: string;
+  usage: string;
+  status: string;
+  responsibleOrganisation: {
+    name: string;
+    uri: string;
+  };
+  publicationDate: string;
+  descriptionFileName: string;
+  specificationDocuments: Document[];
+  documentation: Document[];
+  charter: Document;
+  reports: Document[];
+  presentations: Document[];
+  dateOfRegistration: string;
+  dateOfAcknowledgementByWorkingGroup: string;
+  dateOfAcknowledgementBySteeringCommittee: string;
+  datePublicReviewStart: string;
+  datePublicReviewEnd: string;
+  endOfPublicationDate: string;
+}
+
+interface Document {
+  name?: string;
+  resourceReference?: string;
+}
